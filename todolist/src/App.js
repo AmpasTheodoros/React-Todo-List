2749ad0b1a249dd0e9a96eb03695b038dccc9e8c
TheodorAmpas <ampastheodwros@gmail.com>
2022-10-25 22:58:28 +0300

    commit

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Netchy's Todo List</h1>
+        <h1>Theodor's Todo List</h1>
       </header>
       <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus}  />
 
